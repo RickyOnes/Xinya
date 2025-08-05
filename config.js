4f3d877f2d290881478cd48c101048d09d81dd66
RickyOne <162004332@qq.com>
2025-08-05 19:12:36 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+
+window.config = {
+  SUPABASE_URL: "https://iglmqwpagzjadwauvchh.supabase.co",
+  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlnbG1xd3BhZ3pqYWR3YXV2Y2hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4ODk4NDAsImV4cCI6MjA2NjQ2NTg0MH0.Mtiwp31mJvbLRTotbrb4_DobjjpM4kg9f4-G8oWz85E"
+};
