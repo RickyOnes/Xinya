4cb458f632c45829662ca423ff600afc0ba1704d
RickyOne <162004332@qq.com>
2025-08-10 00:49:39 +0800

    Add files via upload

---
+++ zh.js
@@ -0,0 +1,42 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.zh = {}));
+}(this, (function (exports) { 'use strict';
+
+  var fp = typeof window !== "undefined" && window.flatpickr !== undefined
+      ? window.flatpickr
+      : {
+          l10ns: {},
+      };
+  var Mandarin = {
+      weekdays: {
+          shorthand: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
+          longhand: [
+              "星期日",
+              "星期一",
+              "星期二",
+              "星期三",
+              "星期四",
+              "星期五",
+              "星期六",
+          ],
+      },
+      months: {
+          shorthand: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+          longhand: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+      },
+      rangeSeparator: " ~ ",
+      weekAbbreviation: "周",
+      scrollTitle: "滚动切换",
+      toggleTitle: "点击切换 12/24 小时时制",
+  };
+  fp.l10ns.zh = Mandarin;
+  var zh = fp.l10ns;
+
+  exports.Mandarin = Mandarin;
+  exports.default = zh;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
